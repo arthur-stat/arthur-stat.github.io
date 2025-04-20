6576ec87961628cb45b6eaeec48facae5b652ad8
arthur-stat <asheovo@gmail.com>
2025-04-21 00:39:03 +0800

    Site updated: 2025-04-21 00:39:00

---
+++ 2025/04/13/计算机网络/aaa_config.py
@@ -0,0 +1,8 @@
+SETTINGS = {
+    "input_folder": "E:\\blog\\source\\_posts\\计算机网络",
+    "output_folder": "",
+    "input_resolution": (2560, 1440),
+    "output_resolution": (1920, 1080),
+    "compaction_quality": 90,
+    "clear_original_files": True
+}
